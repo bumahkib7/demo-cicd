2c2599cb02241a0d2fd313affc15c12f6196cd2c
Bukhari bin mahmoud Kibuka <93518580+bumahkib7@users.noreply.github.com>
2022-12-01 11:26:35 +0100

    Initial Commit

---
+++ src/test/java/code/refractor/DemoCicdTest.java
@@ -0,0 +1,21 @@
+package code.refractor;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class DemoCicdTest {
+
+  @Inject
+  EmbeddedApplication<?> application;
+
+  @Test
+  void testItWorks() {
+    Assertions.assertTrue(application.isRunning());
+  }
+
+}
